656b4b291e281a0337f7a2f2cada2a8524296c2b
Nukleus <stefanvalouch@googlemail.com>
2009-02-22 14:00:40 +0100

    Add Patricks oidentd spoofing module.

---
+++ psotnic-current/modules/oidentd/oidentd.cpp
@@ -0,0 +1,151 @@
+// oidentd spoofing module for psotnic
+
+// - configuration -
+
+// method 1: define this, if you have only one bot on your shell or multiple bots that should use the same ident
+#define OS_METHOD1
+
+// method 2: define this, if you have multiple bots on your shell and each bot should use a different ident
+//#define OS_METHOD2
+
+// define this, if your oidentd config is not located in $HOME/.oidentd.conf
+//#define OS_CONFIG "/home/patrick/.oidentd.conf"
+
+// reconnect if ident set in config and the one the bot actually got are not the same
+//#define OS_RECONNECT
+
+// oidentd.conf will be deleted during this period (for method2)
+#define OS_TIDY_UP_MIN_DELAY 180
+#define OS_TIDY_UP_MAX_DELAY 600
+
+// - end -
+
+#if (defined (OS_METHOD1) && defined (OS_METHOD2)) || (!defined (OS_METHOD1) && !defined (OS_METHOD2))
+#error You must define OS_METHOD1 or OS_METHOD2
+#endif
+
+#include "../prots.h"
+#include "../global-var.h"
+
+char oidentd_cfg[MAX_LEN];
+time_t oidentd_tidy_up_time;
+
+void oidentd_spoofing();
+
+void hook_connecting()
+{
+    oidentd_spoofing();
+}
+
+#ifdef OS_METHOD1
+void oidentd_spoofing()
+{
+    FILE *fh;
+
+    if((fh=fopen(oidentd_cfg, "w")))
+    {
+        fprintf(fh, "global { reply \"%s\" }\n", (const char *) config.ident);
+        fclose(fh);
+    }
+
+    else
+        net.send(HAS_N, "[\002oidentd\002] cannot open '", oidentd_cfg, "'. oidentd spoofing will not work.", NULL);
+}
+#endif
+
+#ifdef OS_METHOD2
+void oidentd_spoofing()
+{
+    FILE *fh;
+    char *lport=net.irc.getMyPortName();
+
+    if(!lport || !strcmp(lport, "0"))
+    {
+        net.send(HAS_N, "[\002oidentd\002] getMyPortName() failed. oidentd spoofing will not work.", NULL);
+        return;
+    }
+
+    if((fh=fopen(oidentd_cfg, "a")))
+    {
+        fprintf(fh, "lport %s { reply \"%s\" }\n", lport, (const char *) config.ident);
+        fclose(fh);
+        oidentd_tidy_up_time=NOW+OS_TIDY_UP_MIN_DELAY+rand()/(RAND_MAX/(OS_TIDY_UP_MAX_DELAY-OS_TIDY_UP_MIN_DELAY)+1);
+    }
+
+    else
+        net.send(HAS_N, "[\002oidentd\002] cannot open '", oidentd_cfg, "'. oidentd spoofing will not work.", NULL);
+}
+
+void hook_timer()
+{
+    struct stat sb;
+
+    if(!oidentd_tidy_up_time)
+        return;
+
+    if(NOW>=oidentd_tidy_up_time)
+    {
+        if(stat(oidentd_cfg, &sb)==-1)
+            return;
+
+        // do not delete, if the file has been modified lately
+        if(NOW-sb.st_mtime < OS_TIDY_UP_MIN_DELAY)
+        {
+            oidentd_tidy_up_time+=OS_TIDY_UP_MIN_DELAY-(NOW-sb.st_mtime);
+            return;
+        }
+
+        unlink(oidentd_cfg);
+        oidentd_tidy_up_time=0;
+    }
+}
+#endif
+
+#ifdef OS_RECONNECT
+void hook_connected()
+{
+    if(!match(ME.ident, config.ident))
+    {
+        net.send(HAS_N, "[\002oidentd\002] I did not get the ident I wanted. reconnecting..", NULL);
+        net.irc.send("QUIT :reconnecting", NULL);
+        net.irc.close("reconnecting");
+        ME.nextConnToIrc=NOW+set.IRC_CONN_DELAY;
+    }
+}
+#endif
+
+extern "C" module *init()
+{
+    module *m = new module("oidentd spoofing", "patrick <patrick@psotnic.com>", "0.1");
+
+#ifdef OS_CONFIG
+    strncpy(oidentd_cfg, OS_CONFIG, MAX_LEN);
+#else
+    struct passwd *pw;
+
+    if(!(pw=getpwuid(getuid())))
+    {
+        net.send(HAS_N, "[\002oidentd\002] getpwuid() failed. oidentd spoofing will not work", NULL); 
+        return m;
+    }
+
+    snprintf(oidentd_cfg, MAX_LEN, "%s/.oidentd.conf", pw->pw_dir);
+#endif
+
+    m->hooks->connecting=hook_connecting;
+
+#ifdef OS_RECONNECT
+    m->hooks->connected=hook_connected;
+#endif
+
+#ifdef OS_METHOD2
+    m->hooks->timer=hook_timer;
+#endif
+
+    oidentd_tidy_up_time=0;
+    return m;
+}
+
+extern "C" void destroy()
+{
+}
